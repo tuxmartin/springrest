8227ade3492fe99fdd3eea7d97f6111f94020954
Martin Vancl <tux.martin@gmail.com>
2014-06-23 13:25:27 +0200

    Zmenena anotace z @Controller na @RestController pro JSON controller.

--- src/main/java/eu/vancl/martin/springrest/controller/JSONController.java
+++ src/main/java/eu/vancl/martin/springrest/controller/JSONController.java
@@ -6,17 +6,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import eu.vancl.martin.springrest.entity.Role;
 import eu.vancl.martin.springrest.service.RoleManager;
 
-@Controller
+@RestController // http://goo.gl/Lo974a
 @RequestMapping("/role")
 public class JSONController {
 	
