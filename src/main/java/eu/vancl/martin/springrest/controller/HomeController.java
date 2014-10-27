035fdcacd15574c91ed59509aaec5edb3f00f043
Martin Vancl <tux.martin@gmail.com>
2014-10-27 23:28:28 +0100

    +vytvareni testovacich roli

--- src/main/java/eu/vancl/martin/springrest/controller/HomeController.java
+++ src/main/java/eu/vancl/martin/springrest/controller/HomeController.java
@@ -31,11 +31,27 @@ public class HomeController {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		/* pro testovani */
+		
+		if (roleManager.findByName("ROLE_ADMIN") == null) {
+			Role admin = new Role();
+			admin.setId(1);
+			admin.setName("ROLE_ADMIN");		
+			roleManager.save(admin);
+		}
+		
+		if (roleManager.findByName("ROLE_USER") == null) {
+			Role user = new Role();
+			user.setId(2);
+			user.setName("ROLE_USER");
+			roleManager.save(user);
+		}
+		
 		if (userManager.findByUserName("admin@example.net") == null) {
 			User admin = new User();
 			admin.setUsername("admin@example.net");
 			admin.setPassword("8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"); // 123456
 			admin.setEnabled(true);
+			admin.setRole(roleManager.findByName("ROLE_ADMIN"));
 			userManager.save(admin);
 			logger.info("admin user created: " + admin.toString());
 		}
@@ -45,15 +61,13 @@ public class HomeController {
 			user.setUsername("user");
 			user.setPassword("123456"); // 123456
 			user.setEnabled(true);
+			user.setRole(roleManager.findByName("ROLE_USER"));
 			userManager.save(user);
 			logger.info("normal user created: " + user.toString());
 		}	
 
-		Role r = roleManager.findById(1);
-		if (r != null) {
-			logger.info("___" + r.getName());	
-			model.addAttribute("role", r);
-		}	
+		logger.info("___" + roleManager.findById(0));	
+		model.addAttribute("role", roleManager.findById(0));
 		/* pro testovani */
 
 		model.addAttribute("vsechnyRole", roleManager.findAll());
